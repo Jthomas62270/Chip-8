a1e83ae5d8595047eb163193a2d4da700b6d3a6e
Jthomas62270 <Jthomas62270@gmail.com>
2024-06-14 10:55:36 -0400

    platform started

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <fstream>
 #include <chrono>
 #include <random> 
+#include <cstring> 
+#include <iostream>
+#include <SDL.h>
+
 
 const unsigned int START_ADDRESS = 0x200; 
 const unsigned int FONTSET_SIZE = 80; 
@@ -35,7 +39,92 @@ class Chip8 {
         : randGen(std::chrono::system_clock::now().time_since_epoch().count())
         {
             randByte = std::uniform_int_distribution<uint8_t>(0, 255U);
+
+            table[0x0] = &Chip8::Table0;
+            table[0x1] = &Chip8::OP_1nnn;
+            table[0x2] = &Chip8::OP_2nnn;
+            table[0x3] = &Chip8::OP_3xkk;
+            table[0x4] = &Chip8::OP_4xkk;
+            table[0x5] = &Chip8::OP_5xy0;
+            table[0x6] = &Chip8::OP_6xkk;
+            table[0x7] = &Chip8::OP_7xkk;
+            table[0x8] = &Chip8::Table8;
+            table[0x9] = &Chip8::OP_9xy0;
+            table[0xA] = &Chip8::OP_Annn;
+            table[0xB] = &Chip8::OP_Bnnn;
+            table[0xC] = &Chip8::OP_Cxkk;
+            table[0xD] = &Chip8::OP_Dxyn;
+            table[0xE] = &Chip8::TableE;
+            table[0xF] = &Chip8::TableF;
+
+            for (size_t i = 0; i <= 0xE; i++)
+            {
+                table0[i] = &Chip8::OP_NULL;
+                table8[i] = &Chip8::OP_NULL;
+                tableE[i] = &Chip8::OP_NULL;
+            }
+
+            table0[0x0] = &Chip8::OP_00E0;
+            table0[0xE] = &Chip8::OP_00EE;
+
+            table8[0x0] = &Chip8::OP_8xy0;
+            table8[0x1] = &Chip8::OP_8xy1;
+            table8[0x2] = &Chip8::OP_8xy2;
+            table8[0x3] = &Chip8::OP_8xy3;
+            table8[0x4] = &Chip8::OP_8xy4;
+            table8[0x5] = &Chip8::OP_8xy5;
+            table8[0x6] = &Chip8::OP_8xy6;
+            table8[0x7] = &Chip8::OP_8xy7;
+            table8[0xE] = &Chip8::OP_8xyE;
+
+            tableE[0x1] = &Chip8::OP_ExA1;
+            tableE[0xE] = &Chip8::OP_Ex9E;
+
+            for (size_t i = 0; i <= 0x65; i++)
+            {
+                tableF[i] = &Chip8::OP_NULL;
+            }
+
+            tableF[0x07] = &Chip8::OP_Fx07;
+            tableF[0x0A] = &Chip8::OP_Fx0A;
+            tableF[0x15] = &Chip8::OP_Fx15;
+            tableF[0x18] = &Chip8::OP_Fx18;
+            tableF[0x1E] = &Chip8::OP_Fx1E;
+            tableF[0x29] = &Chip8::OP_Fx29;
+            tableF[0x33] = &Chip8::OP_Fx33;
+            tableF[0x55] = &Chip8::OP_Fx55;
+            tableF[0x65] = &Chip8::OP_Fx65;
         }
+
+        void Table0()
+        {
+            ((*this).*(table0[opcode & 0x000Fu]))();
+        }
+
+        void Table8()
+        {
+            ((*this).*(table8[opcode & 0x000Fu]))();
+        }
+
+        void TableE()
+        {
+            ((*this).*(tableE[opcode & 0x000Fu]))();
+        }
+
+        void TableF()
+        {
+            ((*this).*(tableF[opcode & 0x00FFu]))();
+        }
+
+        void OP_NULL()
+        {}
+
+    typedef void (Chip8::*Chip8Func)();
+	Chip8Func table[0xF + 1];
+	Chip8Func table0[0xE + 1];
+	Chip8Func table8[0xE + 1];
+	Chip8Func tableE[0xE + 1];
+	Chip8Func tableF[0x65 + 1];
     
     std::default_random_engine randGen; 
     std::uniform_int_distribution<uint8_t> randByte; 
@@ -60,9 +149,26 @@ class Chip8 {
 
             free(buffer); 
         }
-    };
+    }
 
-    //
+    void Chip8::Cycle()
+    {
+        opcode = (memory[pc] << 8u) | memory[pc +1]; 
+
+        pc += 2; 
+
+        ((*this).*(table[(opcode & 0x0F000u) >> 12u]))(); 
+
+        if(delayTimer > 0)
+        {
+            --delayTimer; 
+        }
+
+        if(soundTimer > 0)
+        {
+            --soundTimer;
+        }
+    }
 
     uint8_t fontset[FONTSET_SIZE] = 
     { 
@@ -84,7 +190,12 @@ class Chip8 {
         0xF0, 0x80, 0xF0, 0x80, 0x80  // F
     };
 
-    //Instruction Sets
+
+    /*
+    
+        Instruction Sets
+    
+    */
 
     //00E0: CLS - Clear the display
     void Chip8::OP_00E0()
@@ -107,7 +218,7 @@ class Chip8 {
     }
 
     //2nnn: Call - Call to subroutine at nnn
-    void Chip8::oP_2nnn()
+    void Chip8::OP_2nnn()
     { 
         uint16_t address = opcode  & 0x0FFFu; 
 
@@ -452,7 +563,7 @@ class Chip8 {
     }
 
     //Fx18: LD ST, Vx - Set sound timer = Vx
-    void Chip8::OP_Fx15()
+    void Chip8::OP_Fx18()
     { 
         uint8_t Vx = (opcode & 0x0F00u) >> 8u; 
 
@@ -460,7 +571,7 @@ class Chip8 {
     } 
 
     //Fx1E: ADD I, Vx - set I = I + Vx
-    void Chip8::OP_Fx1e()
+    void Chip8::OP_Fx1E()
     { 
         uint8_t Vx = (opcode &0x0F00u) >> 8u; 
 
@@ -493,7 +604,7 @@ class Chip8 {
     }
 
     //Fx55: LD [I], Vx - Stores registers V0 through Vx in memory location starting at location I
-    void Chip8::OP_F55()
+    void Chip8::OP_Fx55()
     { 
         uint8_t Vx = (opcode & 0x0F00u) >> 8u;
         for (uint8_t i = 0; i <= Vx; ++i) 
@@ -513,7 +624,14 @@ class Chip8 {
         }
     }
 
-
 };
 
+class Platform 
+{ 
+    public: 
+    Platform(char const* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight)
+    {
+        SDL_Init(SDL_INIT_VIDEO); 
+    }
+};
 
